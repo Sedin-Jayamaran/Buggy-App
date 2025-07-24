5244cb77c87f63c4b10f2f1b1995c6b3adefa7ca
jayamaran <jayamaran@sedintechnologies.com>
2025-07-24 11:32:04 +0000

    new

--- Dockerfile.app
+++ Dockerfile.app
@@ -4,6 +4,9 @@ COPY Gemfile Gemfile.lock ./
 RUN bundle config set --local without 'development test' && \
     bundle install --jobs 4 --retry 3
 COPY . .
-RUN bundle exec rake assets:precompile
+
+RUN RAILS_ENV=production bundle exec rails assets:precompile
+ 
 EXPOSE 3000
-CMD ["bundle", "exec", "puma", "-C", "config/puma.rb"]
+ 
+CMD ["bash", "-c", "bundle exec rails db:migrate && rails server -b 0.0.0.0"]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local = false
 
   # Turn on fragment caching in view templates.
-  config.action_controller.perform_caching = 
+  config.action_controller.perform_caching = true
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
@@ -33,7 +33,8 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger   = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
